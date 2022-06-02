bfdd4a5ee98f03b8ca2a99cfdf8d8b8268c9aae7
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2022-06-02 14:33:06 -0300

    Building Comment Form

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,15 +1,15 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
+  alias Discuss.Repo
+  alias Discuss.Models.Topic
 
-  def join(name, _params, socket) do
-    IO.puts(name)
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    _topic = Repo.get(Topic, topic_id)
     {:ok, %{hey: "there"}, socket}
   end
 
-  def handle_in(name, message, socket) do
-    IO.puts("+++++")
-    IO.puts(name)
-    IO.inspect(message)
+  def handle_in(name, %{"content" => content}, socket) do
     {:reply, :ok, socket}
   end
 
