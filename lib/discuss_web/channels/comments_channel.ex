da9bd8016ab4108b077eba9873478215197d6d77
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2022-06-02 15:03:45 -0300

    Inserting Comments into the database

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,16 +1,28 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
   alias Discuss.Repo
-  alias Discuss.Models.Topic
+  alias Discuss.Models.{Topic, Comment}
 
   def join("comments:" <> topic_id, _params, socket) do
     topic_id = String.to_integer(topic_id)
-    _topic = Repo.get(Topic, topic_id)
-    {:ok, %{hey: "there"}, socket}
+    topic = Repo.get(Topic, topic_id)
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
-  def handle_in(name, %{"content" => content}, socket) do
-    {:reply, :ok, socket}
+  def handle_in(_name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    topic
+    |> Ecto.build_assoc(:comments)
+    |> Comment.changeset(%{content: content})
+    |> save_comment(socket)
+  end
+
+  defp save_comment(changeset, socket) do
+    case Repo.insert(changeset) do
+      {:ok, _comment} -> {:reply, :ok, socket}
+      {:error, _reason} -> {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 
 end
